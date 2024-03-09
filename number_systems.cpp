5df24d09041311fa442a0dbf339ab8d7580a14be
WahomeKennedy <kennedywahome.24@gmail.com>
2024-03-09 13:32:02 +0300

    number systems

---
+++ number_systems.cpp
@@ -0,0 +1,12 @@
+// We have the decimal(base 10), binary(base 2), and hexadecimal (base 16) numbers
+// Hexadecimal numbers are used to represent colors in programming
+//R G B  --> FF 00 00 An example is shown below
+
+#include <iostream>
+using namespace std;
+int main(){
+        int number = 0b11111111;
+        cout << number;
+    return 0;
+}
+
