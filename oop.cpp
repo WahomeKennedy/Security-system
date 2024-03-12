943b8b3f84e00528e24c6574214dfa41bc0842f4
WahomeKennedy <kennedywahome.24@gmail.com>
2024-03-12 19:05:00 +0300

    update oop

--- oop.cpp
+++ oop.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 //using std::string;
 
-class Employee{
+class AbstractEmployee{
+    virtual void AskForPromotion()=0;
+};
+class Employee: AbstractEmployee{
     private:
         string Name;
         string Company;
@@ -15,6 +18,23 @@ class Employee{
         string getName(){
             return Name;
         }
+
+        void setCompany(string company){
+            Company = company;
+        }
+        string getCompany(){
+            return Company;
+        }
+
+        void setAge(int age){
+            if(age>=18){
+            Age = age;
+            }
+        }
+        int getAge(){
+            return Age;
+        }
+
     void introduceYourself(){
         cout << "Name:- "<< Name << endl;
         cout << "Company:- "<< Company << endl;
@@ -25,14 +45,24 @@ class Employee{
         Company = company;
         Age = age;
     }
+    void AskForPromotion(){
+        if (Age>30)
+            cout << Name << " got promoted!" << endl;
+        else
+            cout << Name << " No promotion for you!" << endl;
+    }
 };
 
 int main(){
     Employee employee1 = Employee("Wingflo", "Amazon", 23);
-    employee1.introduceYourself();
+    // employee1.introduceYourself();
 
-    Employee employee2= Employee("Chris", "GitHub", 28);
-    employee2.introduceYourself();
+    Employee employee2= Employee("Chris", "GitHub", 34);
+    // employee2.introduceYourself();
+    employee1.AskForPromotion();
+    employee2.AskForPromotion();
+    // employee1.setAge(14);
+    // cout << employee1.getName()<<" is "<< employee1.getAge()<<" years old"<< endl;
 
     return 0;
 }
